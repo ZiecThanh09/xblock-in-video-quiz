286fa21bc4ac56f2b5cc257733d1a31001b191ac
PhucNhan46 <135731283+PhucNhan46@users.noreply.github.com>
2025-03-13 12:47:59 +0700

    update time setting

--- invideoquiz/public/js/src/invideoquiz.js
+++ invideoquiz/public/js/src/invideoquiz.js
@@ -1,4 +1,4 @@
-/* Javascript for InVideoQuizXBlock. */
+/* Javascript for InVideoQuizXBlock with Countdown Timer */
 function InVideoQuizXBlock(runtime, element) {
   $('.in-video-quiz-block').closest('.vert').hide();
   var videoId = $('.in-video-quiz-block').data('videoid');
@@ -7,181 +7,91 @@ function InVideoQuizXBlock(runtime, element) {
   }
   var problemTimesMap = InVideoQuizXBlock.config[videoId];
   var studentMode = $('.in-video-quiz-block').data('mode') !== 'staff';
-  // var extraVideoButton = '<button class="in-video-continue">Continue</button>';
   var video;
   var videoState;
-
   var knownDimensions;
-  var countdownDuration = 10;
-
-  // Interval at which to check if video size has changed size
-  // and the displayed problems needs to do the same
-  var resizeIntervalTime = 100;
-
-  // Interval at which to check for problems to display
-  // Checking every 0.5 seconds to make sure we check at least once per actual second of video
-  var displayIntervalTime = 500;
-
-  // Timeout to wait before checking for problems again after "play" is clicked
-  // Waiting 1.5 seconds to make sure we are moved to the next second and we don't get a double firing
-  var displayIntervalTimeout = 1500;
-
+  
   $(function () {
       $('#seq_content .vert-mod .vert, #course-content .vert-mod .vert').each(function () {
           var component = $(this);
-
           if (studentMode) {
               setUpStudentView(component);
           } else {
               showProblemTimesToInstructor(component);
           }
       });
-
       if (studentMode) {
-        knownDimensions = getDimensions();
-        bindVideoEvents();
+          knownDimensions = getDimensions();
+          bindVideoEvents();
       }
   });
 
-  // function setUpStudentView(component) {
-  //     var componentIsVideo = component.data('id').indexOf(videoId) !== -1;
-  //     if (componentIsVideo) {
-  //         video = $('.video', component);
-  //     } else {
-  //         $.each(problemTimesMap, function (time, componentId) {
-  //             if (component.data('id').indexOf(componentId) !== -1) {
-  //                 component.addClass('in-video-problem-wrapper');
-  //                 $('.xblock-student_view', component).append(extraVideoButton).addClass('in-video-problem').hide();
-  //             }
-  //         });
-  //     }
-  // }
   function setUpStudentView(component) {
-    var componentIsVideo = component.data('id').indexOf(videoId) !== -1;
-    if (componentIsVideo) {
-        video = $('.video', component);
-    } else {
-        $.each(problemTimesMap, function (time, componentId) {
-            if (component.data('id').indexOf(componentId) !== -1) {
-                component.addClass('in-video-problem-wrapper');
-                var countdownInput = '<label for="countdown-time">Countdown Time (seconds):</label>' +
-                                     '<input type="number" class="countdown-time" min="1" max="60" value="10">';
-                var countdownTimer = '<div class="countdown-timer">10</div>';
-                $('.xblock-student_view', component).append(countdownInput + countdownTimer).addClass('in-video-problem').hide();
-            }
-        });
-    }
-}
-  function getDimensions() {
-      var position = $('.tc-wrapper', video).position().top;
-      var height = $('.tc-wrapper', video).css('height');
-      var width = $('.tc-wrapper', video).css('width');
-      return {
-        'top': position,
-        'height': height,
-        'width': width
-      };
-  }
-
-  function dimensionsHaveChanged(newDimensions) {
-      for (var key in knownDimensions) {
-          if (newDimensions.hasOwnProperty(key)) {
-              if (knownDimensions[key] !== newDimensions[key]) {
-                  return true;
+      var componentIsVideo = component.data('id').indexOf(videoId) !== -1;
+      if (componentIsVideo) {
+          video = $('.video', component);
+      } else {
+          $.each(problemTimesMap, function (time, componentId) {
+              if (component.data('id').indexOf(componentId) !== -1) {
+                  component.addClass('in-video-problem-wrapper');
+                  var countdownInput = '<label for="countdown-time">Countdown Time (seconds):</label>' +
+                                       '<input type="number" class="countdown-time" min="1" max="60" value="10">';
+                  var countdownTimer = '<div class="countdown-timer">10</div>';
+                  $('.xblock-student_view', component).append(countdownInput + countdownTimer).addClass('in-video-problem').hide();
               }
-          }
+          });
       }
-      return false;
   }
 
-  function showProblemTimesToInstructor(component) {
-      $.each(problemTimesMap, function (time, componentId) {
-          var isInVideoComponent = component.data('id').indexOf(componentId) !== -1;
-          if (isInVideoComponent) {
-              var minutes = parseInt(time / 60, 10);
-              var seconds = ('0' + (time % 60)).slice(-2);
-              var timeParagraph = '<p class="in-video-alert"><i class="fa fa-exclamation-circle"></i>This component will appear in the video at <strong>' + minutes + ':' + seconds + '</strong></p>';
-              component.prepend(timeParagraph);
-          }
-      });
-  }
-
-  function resizeInVideoProblem(currentProblem, dimensions) {
-      currentProblem.css(dimensions);
-  }
-
-  // Bind In Video Quiz display to video time, as well as play and pause buttons
   function bindVideoEvents() {
       var canDisplayProblem = true;
       var intervalObject;
-      var resizeIntervalObject;
       var problemToDisplay;
 
       video.on('play', function () {
-        videoState = videoState || video.data('video-player-state');
-
-        clearInterval(resizeIntervalObject);
-
-        if (problemToDisplay) {
-          window.setTimeout(function () {
-            canDisplayProblem = true;
-          }, displayIntervalTimeout);
-          problemToDisplay.hide();
-          problemToDisplay = null;
-        }
-
-        intervalObject = setInterval(function () {
-          var videoTime = parseInt(videoState.videoPlayer.currentTime, 10);
-          var problemToDisplayId = problemTimesMap[videoTime];
-          if (problemToDisplayId && canDisplayProblem) {
-            $('.wrapper-downloads, .video-controls', video).hide();
-            $('#seq_content .vert-mod .vert, #course-content .vert-mod .vert').each(function () {
-              var isProblemToDisplay = $(this).data('id').indexOf(problemToDisplayId) !== -1;
-              if (isProblemToDisplay) {
-                problemToDisplay = $('.xblock-student_view', this)
-                videoState.videoPlayer.pause();
-                resizeInVideoProblem(problemToDisplay, getDimensions());
-                problemToDisplay.show();
-                problemToDisplay.css({display: 'block'});
-                canDisplayProblem = false;
-              }
-            });
+          videoState = videoState || video.data('video-player-state');
+          if (problemToDisplay) {
+              problemToDisplay.hide();
+              problemToDisplay = null;
+              canDisplayProblem = true;
           }
-        }, displayIntervalTime);
-      });
-
-      video.on('pause', function () {
-        videoState = videoState || video.data('video-player-state');
-        clearInterval(intervalObject);
-        if (problemToDisplay) {
-          resizeIntervalObject = setInterval(function () {
-            var currentDimensions = getDimensions();
-            if (dimensionsHaveChanged(currentDimensions)) {
-                  resizeInVideoProblem(problemToDisplay, currentDimensions);
-                  knownDimensions = currentDimensions;
-            }
-          }, resizeIntervalTime);
-          $('.in-video-continue', problemToDisplay).on('click', function () {
-            $('.wrapper-downloads, .video-controls', video).show();
-            videoState.videoPlayer.play();
-          });
-        }
+          intervalObject = setInterval(function () {
+              var videoTime = parseInt(videoState.videoPlayer.currentTime, 10);
+              var problemToDisplayId = problemTimesMap[videoTime];
+              if (problemToDisplayId && canDisplayProblem) {
+                  $('.wrapper-downloads, .video-controls', video).hide();
+                  $('#seq_content .vert-mod .vert, #course-content .vert-mod .vert').each(function () {
+                      if ($(this).data('id').indexOf(problemToDisplayId) !== -1) {
+                          problemToDisplay = $('.xblock-student_view', this);
+                          videoState.videoPlayer.pause();
+                          problemToDisplay.show();
+                          
+                          var countdownTime = parseInt($('.countdown-time', problemToDisplay).val(), 10) || 10;
+                          console.log("Countdown Time:", countdownTime); // Debugging
+                          startCountdown(problemToDisplay, countdownTime);
+                          
+                          canDisplayProblem = false;
+                      }
+                  });
+              }
+          }, 500);
       });
   }
-  function startCountdown(problemElement, duration) {
-    var timerElement = $('.countdown-timer', problemElement);
-    var timeLeft = duration;
-    timerElement.text(timeLeft);
 
-    var countdown = setInterval(function () {
-        timeLeft--;
-        timerElement.text(timeLeft);
-        if (timeLeft <= 0) {
-            clearInterval(countdown);
-            problemElement.hide();
-            $('.wrapper-downloads, .video-controls', video).show();
-            videoState.videoPlayer.play();
-        }
-    }, 1000);
-}
+  function startCountdown(problemElement, duration) {
+      var timerElement = $('.countdown-timer', problemElement);
+      var timeLeft = duration;
+      timerElement.text(timeLeft);
+
+      var countdown = setInterval(function () {
+          timeLeft--;
+          timerElement.text(timeLeft);
+          if (timeLeft <= 0) {
+              clearInterval(countdown);
+              problemElement.fadeOut();
+              $('.wrapper-downloads, .video-controls', video).show();
+              videoState.videoPlayer.play();
+          }
+      }, 1000);
+  }
 }
