60639454dc801277f3c75c7b5c3b72d00caa434a
PhucNhan46 <135731283+PhucNhan46@users.noreply.github.com>
2025-03-13 15:13:12 +0700

    update code js

--- invideoquiz/public/js/src/invideoquiz.js
+++ invideoquiz/public/js/src/invideoquiz.js
@@ -11,15 +11,21 @@ function InVideoQuizXBlock(runtime, element) {
   var videoState;
   var knownDimensions;
 
+  var resizeIntervalTime = 100;
+  var displayIntervalTime = 500;
+  var displayIntervalTimeout = 1500;
+
   $(function () {
       $('#seq_content .vert-mod .vert, #course-content .vert-mod .vert').each(function () {
           var component = $(this);
+
           if (studentMode) {
               setUpStudentView(component);
           } else {
               showProblemTimesToInstructor(component);
           }
       });
+
       if (studentMode) {
           knownDimensions = getDimensions();
           bindVideoEvents();
@@ -43,38 +49,82 @@ function InVideoQuizXBlock(runtime, element) {
       }
   }
 
+  function getDimensions() {
+    var position = $('.tc-wrapper', video).position().top;
+    var height = $('.tc-wrapper', video).css('height');
+    var width = $('.tc-wrapper', video).css('width');
+    return {
+      'top': position,
+      'height': height,
+      'width': width
+    };
+  }
+
+  function dimensionsHaveChanged(newDimensions) {
+    for (var key in knownDimensions) {
+        if (newDimensions.hasOwnProperty(key)) {
+            if (knownDimensions[key] !== newDimensions[key]) {
+                return true;
+            }
+        }
+    }
+    return false;
+  } 
+
+  function showProblemTimesToInstructor(component) {
+    $.each(problemTimesMap, function (time, componentId) {
+        var isInVideoComponent = component.data('id').indexOf(componentId) !== -1;
+        if (isInVideoComponent) {
+            var minutes = parseInt(time / 60, 10);
+            var seconds = ('0' + (time % 60)).slice(-2);
+            var timeParagraph = '<p class="in-video-alert"><i class="fa fa-exclamation-circle"></i>This component will appear in the video at <strong>' + minutes + ':' + seconds + '</strong></p>';
+            component.prepend(timeParagraph);
+        }
+    });
+  }
+
+  function resizeInVideoProblem(currentProblem, dimensions) {
+      currentProblem.css(dimensions);
+  }
+
   function bindVideoEvents() {
       var canDisplayProblem = true;
       var intervalObject;
+      var resizeIntervalObject;
       var problemToDisplay;
 
       video.on('play', function () {
           videoState = videoState || video.data('video-player-state');
           if (problemToDisplay) {
-              problemToDisplay.hide();
-              problemToDisplay = null;
+            window.setTimeout(function () {
               canDisplayProblem = true;
+            }, displayIntervalTimeout);
+            problemToDisplay.hide();
+            problemToDisplay = null;
           }
+
           intervalObject = setInterval(function () {
-              var videoTime = parseInt(videoState.videoPlayer.currentTime, 10);
-              var problemToDisplayId = problemTimesMap[videoTime];
-              if (problemToDisplayId && canDisplayProblem) {
-                  $('.wrapper-downloads, .video-controls', video).hide();
-                  $('#seq_content .vert-mod .vert, #course-content .vert-mod .vert').each(function () {
-                      if ($(this).data('id').indexOf(problemToDisplayId) !== -1) {
+            var videoTime = parseInt(videoState.videoPlayer.currentTime, 10);
+            var problemToDisplayId = problemTimesMap[videoTime];
+            if (problemToDisplayId && canDisplayProblem) {
+              $('.wrapper-downloads, .video-controls', video).hide();
+              $('#seq_content .vert-mod .vert, #course-content .vert-mod .vert').each(function () {
+                var isProblemToDisplay = $(this).data('id').indexOf(problemToDisplayId) !== -1;
+                if (isProblemToDisplay) {
                           problemToDisplay = $('.xblock-student_view', this);
                           videoState.videoPlayer.pause();
+                          resizeInVideoProblem(problemToDisplay, getDimensions());
                           problemToDisplay.show();
-                          
+                          problemToDisplay.css({display: 'block'});
                           var countdownTime = parseInt($('.countdown-time', problemToDisplay).val(), 10) || 10;
-                          console.log("Countdown Time:", countdownTime); // Debugging
+                          console.log("Countdown Time:", countdownTime);
                           startCountdown(problemToDisplay, countdownTime);
                           
                           canDisplayProblem = false;
                       }
                   });
               }
-          }, 500);
+          }, displayIntervalTime);
       });
   }
 
